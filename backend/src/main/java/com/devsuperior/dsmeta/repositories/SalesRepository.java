698321d23cb891cc900c765450da3cf08293d961
mauricio-sponton <sponton.lol@gmail.com>
2022-07-14 01:24:51 -0300

    Filtro por data

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -8,4 +13,6 @@ import com.devsuperior.dsmeta.entities.Sale;
 @Repository
 public interface SalesRepository extends JpaRepository<Sale,Long >{
 
+	@Query("SELECT s FROM Sale s WHERE s.date BETWEEN :min AND :max ORDER BY s.amount DESC")
+	Page<Sale> findSalesByDate(LocalDate min, LocalDate max, Pageable pageable);
 }
