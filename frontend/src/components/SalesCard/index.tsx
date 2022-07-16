bee10895c0100c8624f9ef301afd157d53b16a2f
mauricio-sponton <sponton.lol@gmail.com>
2022-07-15 23:01:51 -0300

    primeira requisição usando Axios

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,12 +3,19 @@ import "./styles.css";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SalesCard() {
 
 const [minDate, setMinDate] = useState(new Date());
 const [maxDate, setMaxDate] = useState(new Date());
+
+useEffect(() => {
+  axios.get("http://localhost:8080/sales").then(response => {
+    console.log(response.data);
+  });
+}, []);
 
   return (
     <>
